0da408fe82dc7a4f1fd505ec2eebf4e875be2b8f
Behnam Sheykhe <learndlt@gmail.com>
2020-04-02 20:39:41 +0430

    add borders

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: true,
+    width,
+    height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+// Walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+];
+// Maze generation
+s
